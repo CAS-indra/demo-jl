135b6fd8755094c53d4fb50975388420a664d106
Jose Luis Mendoza Albaladejo <joseluismendoza97@gmail.com>
2021-09-16 17:53:44 +0200

    crear cabecera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutModule } from './layout/layout.module';
 import { TituloComponent } from './titulo/titulo.component';
 
 @NgModule({
   declarations: [AppComponent, TituloComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
