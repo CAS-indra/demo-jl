82b59a929150402fc352c75e8c0594c350a829ce
Jose Luis Mendoza Albaladejo <joseluismendoza97@gmail.com>
2021-09-17 08:55:18 +0200

    cambios en template y styles del archivo "titulo"

--- src/app/titulo/titulo.component.ts
+++ src/app/titulo/titulo.component.ts
@@ -2,8 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-titulo',
-  templateUrl: './titulo.component.html',
-  styleUrls: ['./titulo.component.scss'],
+  template: `<h1>{{ title }} funciona</h1>`,
+  // templateUrl: './titulo.component.html',
+  //styleUrls: ['./titulo.component.scss'],
+  styles: [
+    `
+      h1 {
+        color: blue;
+      }
+    `,
+  ],
 })
 export class TituloComponent {
   title = 'App demo de JL';
