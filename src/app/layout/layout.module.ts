2b3d5c937d3a51d357bec79e97c7f0e9d556cab6
Jose Luis Mendoza Albaladejo <joseluismendoza97@gmail.com>
2021-09-17 14:15:58 +0200

    routing

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { DesarrolladorComponent } from './desarrollador.component';
 import { FechaComponent } from './fecha.component';
+import { Router, RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [CabeceraComponent, DesarrolladorComponent, FechaComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [CabeceraComponent, DesarrolladorComponent, FechaComponent],
 })
 export class LayoutModule {}
